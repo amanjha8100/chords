f78bc8fea98013dcdcc5ca7127e62e595946cbe6
Juan Ignacio Battiston <juan.i.b02@hotmail.com>
2021-10-25 12:07:08 -0300

    General project restructure.
    Removed duplicated bot constructor.
    Fixed prefix.txt not being used at all.
    Renamed app.py to bot.py (Will make more sense in future prs).
    Minor formatting.

--- bot.py
+++ bot.py
@@ -3,20 +3,23 @@ from music import Music
 import os
 import discord
 from discord.ext import commands
+import json
 
 from dotenv import load_dotenv
+
 load_dotenv()
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    prefix = config["PREFIX"]
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="_", intents=intents)
-bot.remove_command("help")
-with open('prefix.txt', 'r') as fp:
-    prefix = fp.read()
-
 bot = commands.Bot(command_prefix=prefix, intents=intents)
-bot.remove_command('help')
+bot.remove_command("help")
+
+
 bot.add_cog(Music(bot))
 bot.add_cog(Help(bot))
 
